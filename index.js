33f9715399be78492ded2052ad03462820c3ff6a
Roman Tsiselsky <rromann4k2030@gmail.com>
2022-04-30 21:13:58 +0300

    Version1.9

--- index.js
+++ index.js
@@ -1,14 +1,37 @@
-const welcome = person => {
-    console.log(`Ave, ${person.name}!`);
+const SALUTATION = 'Ave';
+
+const COLORS = [
+    'black',
+    'red',
+    'green',
+    'yellow',
+    'blue',
+    'magenta',
+    'cyan',
+    'white'
+];
+
+const colorer = (s, color) => `\x1b[3${color}m${s}\x1b[0m`;
+
+const colorize = name => {
+    let res = '';
+    const letters = name.split('');
+    let color = 1;
+    for (const letter of letters){
+        res += colorer(letter, color++);
+        if(color > COLORS.length) color = 1;
+    }
+    return res;
 };
 
-const persons = {
-   marcus: {name: "Marcus Aurelius"},
-   mao: {name: 'Mao Zedong'},
-   rene: {name: 'Rene Descartes'},
-};
+const greetings = name => (
+    name.includes('Augustus')?
+    `${SALUTATION}, ${colorize(name)}!` :
+    `Hello, ${name}!`
+);
 
-for (const name in persons){
-    const person = persons[name];
-    welcome(person);
-}
+const fullName = 'Marcus Aurelius Antoninus Augustus';
+console.log(greetings(fullName));
+
+const shortName = 'Marcus Aurelius';
+console.log(greetings(shortName));
