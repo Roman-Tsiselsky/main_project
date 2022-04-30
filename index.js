97988299731a671056d0b7925fac9f9726ead128
Roman Tsiselsky <rromann4k2030@gmail.com>
2022-05-01 00:04:41 +0300

    Version2.0

--- index.js
+++ index.js
@@ -1,37 +1,35 @@
-const SALUTATION = 'Ave';
-
-const COLORS = [
-    'black',
-    'red',
-    'green',
-    'yellow',
-    'blue',
-    'magenta',
-    'cyan',
-    'white'
-];
-
-const colorer = (s, color) => `\x1b[3${color}m${s}\x1b[0m`;
-
-const colorize = name => {
-    let res = '';
-    const letters = name.split('');
-    let color = 1;
-    for (const letter of letters){
-        res += colorer(letter, color++);
-        if(color > COLORS.length) color = 1;
+const adder = (initial = 0) => ({
+    value: initial,
+    steps: [initial],
+    add(value){
+        this.steps.push(value);
+        this.value += value;
+        return this;
     }
-    return res;
-};
+});
 
-const greetings = name => (
-    name.includes('Augustus')?
-    `${SALUTATION}, ${colorize(name)}!` :
-    `Hello, ${name}!`
-);
+const Adder = class {
+    constructor(initial = 0){
+        this.steps = [initial];
+        this.value = initial;
+    }
+    add(value){
+        this.steps.push(value);
+        this.value += value;
+        return this;
+    }
+}
 
-const fullName = 'Marcus Aurelius Antoninus Augustus';
-console.log(greetings(fullName));
+{
+    const {value, steps} = adder(5).add(-8).add(11);
+    console.log(value);
+    const [a, b, c] = steps;
+    console.log(a, b, c);
+}
 
-const shortName = 'Marcus Aurelius';
-console.log(greetings(shortName));
+{
+    const{value, steps} = new Adder(5).add(-8).add(11);
+    console.log(value);
+    const [a, b, c] = steps;
+    console.log(a, b, c);
+}
