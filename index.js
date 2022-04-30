bbc582aa8c4e3322cdad1981aa5c76c86ddb45d1
Roman Tsiselsky <rromann4k2030@gmail.com>
2022-04-30 19:58:12 +0300

    Version1.8

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ const welcome = person => {
     console.log(`Ave, ${person.name}!`);
 };
 
-const persons = [
-    {name: "Marcus Aurelius"},
-    {name: 'Mao Zedong'},
-    {name: 'Rene Descartes'},
-];
+const persons = {
+   marcus: {name: "Marcus Aurelius"},
+   mao: {name: 'Mao Zedong'},
+   rene: {name: 'Rene Descartes'},
+};
 
-for (const person of persons){
-welcome(person);
+for (const name in persons){
+    const person = persons[name];
+    welcome(person);
 }
